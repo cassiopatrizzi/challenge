0eb13eca63088334193a00dad327d7817f5e4d47
Cassio Patrizzi <cassio_patrizzi@yahoo.com>
2025-03-13 22:22:23 -0300

    Criacao das funcionalidades

---
+++ app.js
@@ -0,0 +1,47 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeDigitado = document.getElementById('amigo');
+    let nome = nomeDigitado.value.trim();
+
+    if (nome === '') {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+
+    amigos.push(nome);
+    nomeDigitado.value = '';
+    exibirLista();
+}
+
+function exibirLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        let item = document.createElement('li');
+        item.textContent = amigo;
+        lista.appendChild(item);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert('Adicione pelo menos 2 amigos para realizar o sorteio.');
+        return;
+    }
+
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceSorteado];
+
+    exibirSorteio(amigoSorteado); 
+}
+
+function exibirSorteio(amigo) {
+    let resultadoLista = document.getElementById('resultado');
+    resultadoLista.innerHTML = ''; 
+
+    let amigoSorteado = document.createElement('li');
+    amigoSorteado.textContent = amigo;
+    resultadoLista.appendChild(amigoSorteado);
+}
